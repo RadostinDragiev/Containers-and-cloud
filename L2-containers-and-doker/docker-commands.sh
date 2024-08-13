56c37e725ab42d550aa916eaed9bed8e0bd44dfe
Radostin Dragiev <56969624+RadostinDragiev@users.noreply.github.com>
2024-08-14 00:37:59 +0300

    docker-commands.sh added and renamed old one to linux-commands

---
+++ L2-containers-and-doker/docker-commands.sh
@@ -0,0 +1,38 @@
+# Pull image from hub
+docker pull #[image name]
+
+# Lists all local images
+docker images
+
+# Run image
+docker run #[image name]
+
+# Run image with my naming
+docker run --name mycustomname nginxdemos/hello
+
+# Run image with my naming at detached mode
+docker run --name mycustomname -d nginxdemos/hello
+
+# Run image with my naming at detached mode, mapped to port 8080
+docker run --name mycustomname -d -p 8080:80 nginxdemos/hello
+
+# Delete image
+docker rmi #[image name]
+
+# Lists all running containers
+docker ps
+
+# Lists all containers
+docker ps - a
+
+# Stop container
+docker stop #[container ID/name]
+
+# Delete container
+docker rm #[container ID/name]
+
+# Fetch container logs
+docker logs #[container ID/name]
+
+# Fetch container logs in follow mode
+docker logs #[container ID/name] - f
