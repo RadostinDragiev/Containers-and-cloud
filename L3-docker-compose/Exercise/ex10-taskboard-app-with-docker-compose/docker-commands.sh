0edb86ba1af8de63bcdde6d98eb5ec79c0c58be7
Radostin Dragiev <56969624+RadostinDragiev@users.noreply.github.com>
2024-08-22 05:32:47 +0300

    docker compose up ex10

---
+++ L3-docker-compose/Exercise/ex10-taskboard-app-with-docker-compose/docker-commands.sh
@@ -0,0 +1,10 @@
+# Created docker-compose.yaml to ex6-taskboard-app-image-build
+
+# Create network
+docker network create taskboard_network
+
+# Docker compose build
+docker compose build
+
+# Docker compose up
+docker compose up -d
